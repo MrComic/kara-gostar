b0b4ae83f9e790341def6a57895149ea0c06d1d9
MrComic <mohammad.farahani70@gmail.com>
2024-03-17 22:31:17 +0330

    اصلاح config

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,37 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") == "development") {
+    return {
+      connection: {
+        client: "sqlite",
+        connection: {
+          filename: path.join(
+            __dirname,
+            "..",
+            env("DATABASE_FILENAME", ".tmp/data.db")
+          ),
+        },
+        useNullAsDefault: true,
+      },
+    };
+  } else {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "strapi-connector-mysql",
+          settings: {
+            client: "mysql",
+            host: env("DATABASE_HOST", "localhost"),
+            port: env.int("DATABASE_PORT", 3306),
+            database: env("DATABASE_NAME", "karagostar-production"),
+            username: env("DATABASE_USERNAME", "root"),
+            password: env("DATABASE_PASSWORD", ""),
+          },
+          options: {},
+        },
+      },
+    };
+  }
+};
