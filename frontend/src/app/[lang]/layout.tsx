1525848deba74b703845927eeecbd0865c04c923
MrComic <mohammad.farahani70@gmail.com>
2024-03-18 22:53:40 +0330

    test

--- frontend/src/app/[lang]/layout.tsx
+++ frontend/src/app/[lang]/layout.tsx
@@ -101,7 +101,7 @@ export default async function RootLayout({
   // TODO: CREATE A CUSTOM ERROR PAGE
   if (!global) return null;
 
-  const { navbar } = global?.data?.attributes;
+  const { navbar } = global?.data?.attributes || { links: [] };
 
   const navbarLogoUrl = getStrapiMedia(
     navbar?.navbarLogo.logoImg.data?.attributes?.url || ""
